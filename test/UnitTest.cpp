24ee3c526f6a9e619f908617ab2e9a27f706f8f8
vitam <psyviccccc@gmail.com>
2018-02-01 22:46:27 +0100

    separate test module

--- test/UnitTest.cpp
+++ test/UnitTest.cpp
@@ -1,18 +1,3 @@
 #define BOOST_TEST_MODULE DataStructuresUnitTest
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
-#include <iostream>
-
-BOOST_AUTO_TEST_SUITE(TestDriven)
-
-	BOOST_AUTO_TEST_CASE(TestDrivenDev) {
-		BOOST_CHECK_EQUAL(1,1);
-		std::cout << "Test Driven Development" << std::endl;
-	}
-
-	BOOST_AUTO_TEST_CASE(BOOSTVersionCheck) {
-		BOOST_CHECK(BOOST_VERSION >= 106600);
-		std::cout << BOOST_VERSION << std::endl;
-	}
-
-BOOST_AUTO_TEST_SUITE_END()
