99fb8042e5e356de64916a4d531189107b0ceecf
daolao <dalao1002@gmail.com>
2018-06-05 15:15:44 +0200

    remove UpperCase folder

--- Exception/ConcurrentModificationException.h
+++
@@ -1,7 +0,0 @@
-#ifndef ConcurrentModificationException_H
-#define ConcurrentModificationException_H
-#include "Exception.h"
-
-class ConcurrentModificationException final : public Exception {
-};
-#endif
--- Exception/NoSuchElementException.h
+++
@@ -1,7 +0,0 @@
-#ifndef NoSuchElementException_H
-#define NoSuchElementException_H
-#include "Exception.h"
-
-class NoSuchElementException final : public Exception {
-};
-#endif
